3bd9dcae889d86f09e91ebc50f32bbf178f945fd
Deepikasubramanian <deepikasubramanian97@gmail.com>
2020-12-03 13:16:10 +0530

    dashboard changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "https://00762fc5ab31.ngrok.io/api",
+  apiBaseUrl: "https://7e417bb41990.ngrok.io/api",
   apiIconKey: 'lp5fEK6HrlmQQntSsUeO602B0u70UF5wsATHhqA9p9Z2lKydZQ5LYBu3OVGzAlq4'
 };
